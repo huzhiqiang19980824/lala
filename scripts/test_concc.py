371b7955fdce7a804c7d43c89776ba837d5b4c23
huzhiqiang <huzhiqiang@example.com>
2020-03-05 16:12:08 +0800

    hello

--- scripts/test_concc.py
+++ scripts/test_concc.py
@@ -2,6 +2,8 @@ class TestLong:
     def test_long1(self):
         print(1)
     def test_long2(self):
-        print(0)
+        print(1)
     def test_long3(self):
-        Exception(0)
+        print(1)
+    def test_long4(self):
+        assert False
